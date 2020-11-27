5c9020d99f2acf7f642894b88a9608e85fc5f5fa
nadfelipe <nadfelipe.s@gmail.com>
2020-11-27 14:47:21 -0300

    repo init

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import Logo from ''
+
+export default function Nav() {
+  return (
+    <div className="Nav">
+        <header>
+
+        </header>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import App from './App';
+import Home from './pages/home/home.js'
+import reportWebVitals from './reportWebVitals';
+
+const Routes = (
+  <Router basename={process.env.PUBLIC_URL}>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render( Router , document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

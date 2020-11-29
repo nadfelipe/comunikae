47b9e5591fd03172bc4c6661b684965bb5137b30
nadfelipe <nadfelipe.s@gmail.com>
2020-11-28 23:30:30 -0300

    update style nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,37 +5,45 @@ import { Link as SmoothLink  } from "react-scroll";
 import './Nav.css';
 // imagens
 import Logo from './../../assets/img/comunikae_logo.svg'
+import PlayBtnIcon from './../../assets/img/PlayBtnIcon'
 // ícones
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight, faHome, faCompass, faCog, faUser } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default class Nav extends Component{
   render(){
     return (
       <div className="Nav">
           <header>
-            <nav className='main-nav'>
-              <Link to="/">
-                <img src={Logo} alt='Logo do Comunikaê'/>
-              </Link>
-              
+            <nav className='nav'>
               {this.props.setar === 'menu' &&
-                <div id='links-home'>
-                  <SmoothLink to="sobre" smooth={true} className="nav-link">Sobre</SmoothLink>
-                  <SmoothLink to="desafios-missao" smooth={true} className="nav-link">O que queremos?</SmoothLink>
-                  <SmoothLink to="manual" smooth={true} className="nav-link">Como funciona?</SmoothLink>
-                  <SmoothLink to="classes" smooth={true} className="nav-link">Classes</SmoothLink>
-                  <Link to="/login">
-                    {/* TODO: colocar a imagem do coisinho de start */}
-                    <FontAwesomeIcon icon={faCaretRight} />
-                    <p>Entrar ao jogo</p>
-                  </Link>
+                <div className='nav-container-home'>
+                  <div id='links-home'>
+                    <Link to="/">
+                      <img src={Logo} alt='Logo do Comunikaê'/>
+                    </Link>
+                    <div className='nav-content-home'>
+                      <SmoothLink to="sobre" smooth={true} className="nav-link">Sobre</SmoothLink>
+                      <SmoothLink to="desafios-missao" smooth={true} className="nav-link">O que queremos?</SmoothLink>
+                      <SmoothLink to="manual" smooth={true} className="nav-link">Como funciona?</SmoothLink>
+                      <SmoothLink to="classes" smooth={true} className="nav-link">Classes</SmoothLink>
+                    </div>
+                    <Link to="/login" className='btn-entrar'>
+                      <div><PlayBtnIcon className='btn-entrar-icon' fill='red'/></div>
+                      <p>Entrar no jogo</p>
+                    </Link>
+                  </div>
                 </div>
               }
               
               {this.props.setar === 'fora-menu' &&
                 <div id='links-logado'>
                   {/* TODO: alterar as cores pelas props dos icones selecionados  */}
+                  <Link to="/">
+                    <img src={Logo} alt='Logo do Comunikaê'/>
+                  </Link>
                   <Link to="/mural">
                     <FontAwesomeIcon icon={faHome} />
                   </Link>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import './Home.css';
 import Nav from '../../components/Nav/Nav'
-import { render } from '@testing-library/react';
 
 export default class Home extends Component{
 render(){
     return (
       <div className="Home">
-        <Nav setar='fora-menu'></Nav>
+        <Nav setar='menu'></Nav>
         <p>home</p>
+        <section id='sobre'>a</section>
+        <section id='desafios-missao'>a</section>
+        <section id='manual'>a</section>
+        <section id='classes'>a</section>
       </div>
     );
   }

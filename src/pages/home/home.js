f6b95549e186472aa16fe05eb326143150788154
nadfelipe <nadfelipe.s@gmail.com>
2020-12-02 12:09:28 -0300

    up ranking

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,6 +4,7 @@ import Nav from '../../components/Nav/Nav'
 import VNav from '../../components/VerticalNav/VerticalNav'
 import Simbolo from '../../assets/img/comunikae_simbolo.svg'
 import ImgSecao2 from '../../assets/img/imagem-secao-2.png'
+import ImgSecao3 from '../../assets/img/imagem-secao-3.png'
 
 export default class Home extends Component{
 render(){
@@ -24,13 +25,21 @@ render(){
           <section id='sobre'>
             <div className='content-sobre'>
               <div className='sobre-textos'>
-                <h2 className='sobre-h2'>Uma plataforma de gamificação virtual, para incentivar desafios reais nas escolas</h2>
-                <p className='sobre-p'>Queremos melhorar  a interação na escola, estimulando a comunicação, o compartilhamento de projetos, ideias e desafios de forma integrada. Compartilhe o que sua equipe produziu, interaja, conquiste recompensas e pontue cada vez mais.</p>
+                <h2 className='home-h2'>Uma plataforma de gamificação virtual, para incentivar desafios reais nas escolas</h2>
+                <p className='home-p'>Queremos melhorar  a interação na escola, estimulando a comunicação, o compartilhamento de projetos, ideias e desafios de forma integrada. Compartilhe o que sua equipe produziu, interaja, conquiste recompensas e pontue cada vez mais.</p>
               </div>
               <img className='sobre-img' src={ImgSecao2} alt='Cards de desafio e pontuação da equipe'/>
             </div>
           </section>
-          <section id='desafios-missao'>teste seção desafios e missao</section>
+          <section id='desafios-missao'>
+            <div className='content-dm'>
+              <div className='dm-textos'>
+                <h2 className='home-h2'>Se desafie, desafie seus amigos, entre na competição contra outras equipes, coopere, pontue, conquiste.</h2>
+                <p className='home-p'>Os desafios serão criativos, lógicos e até estratégicos, além de alguns serem integrados com suas matérias, tornando o aprendizado mais dinâmico e interessante, tudo para estimular a conexão com você e sua equipe e até para facilitar sua comunicação com os professores.</p>
+              </div>
+              <img className='dm-img' src={ImgSecao3} alt='Imagem do ranking'/>
+            </div>
+          </section>
           <section id='manual'>teste seção manual</section>
           <section id='classes'>teste seção classes</section>
         </div>

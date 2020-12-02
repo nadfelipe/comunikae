61bf4d9d362a8f8716fa7a8b24a88076c0942d25
nadfelipe <nadfelipe.s@gmail.com>
2020-12-02 09:40:51 -0300

    update sobre

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import Nav from '../../components/Nav/Nav'
 import VNav from '../../components/VerticalNav/VerticalNav'
 import Simbolo from '../../assets/img/comunikae_simbolo.svg'
+import ImgSecao2 from '../../assets/img/imagem-secao-2.png'
 
 export default class Home extends Component{
 render(){
@@ -20,7 +21,15 @@ render(){
               <img className='banner-simbolo' src={Simbolo} alt='Símbolo Comunikaê'/>
             </div>
           </section>
-          <section id='sobre'>teste seção sobre</section>
+          <section id='sobre'>
+            <div className='content-sobre'>
+              <div className='sobre-textos'>
+                <h2 className='sobre-h2'>Uma plataforma de gamificação virtual, para incentivar desafios reais nas escolas</h2>
+                <p className='sobre-p'>Queremos melhorar  a interação na escola, estimulando a comunicação, o compartilhamento de projetos, ideias e desafios de forma integrada. Compartilhe o que sua equipe produziu, interaja, conquiste recompensas e pontue cada vez mais.</p>
+              </div>
+              <img className='sobre-img' src={ImgSecao2} alt='Cards de desafio e pontuação da equipe'/>
+            </div>
+          </section>
           <section id='desafios-missao'>teste seção desafios e missao</section>
           <section id='manual'>teste seção manual</section>
           <section id='classes'>teste seção classes</section>

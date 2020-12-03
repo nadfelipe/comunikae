35d7b032bdbab927ea432d5316c3a43779b3838f
nadfelipe <nadfelipe.s@gmail.com>
2020-12-02 22:13:11 -0300

    update manual

--- src/components/Manual/Manual.js
+++ src/components/Manual/Manual.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './Manual.css';
-import conteudo from './contentManual.js'
+import conteudo from './contentManual.js';
+import imagem1 from '../../assets/img/imagem-1.svg';
+import imagem2 from '../../assets/img/imagem-2.png';
+import imagem3 from '../../assets/img/imagem-3.png';
+import imagem4 from '../../assets/img/imagem-4.png';
+import imagem5 from '../../assets/img/imagem-5.png';
 
 export default class Manual extends Component{
     constructor(props) {
@@ -9,7 +14,7 @@ export default class Manual extends Component{
             id: 1,
             titulo: '',
             texto: '',
-            urlImg: null,
+            urlImg: 0,
             nomeContent: '',
             texto2: '',
         }
@@ -29,7 +34,6 @@ export default class Manual extends Component{
         for (let i = 1; i < 6; i++) {
             const element = document.getElementById(`${i}`);
             element.classList.remove('numero-ligado');
-            console.log(element, element.classList)
         }  
     }
 
@@ -37,6 +41,7 @@ export default class Manual extends Component{
         let conteudoBuscado = conteudo.contentManual.find(item => item.id === id)
 
         this.setState({
+            id: conteudoBuscado.id,
             titulo: conteudoBuscado.titulo,
             texto: conteudoBuscado.texto,
             urlImg: conteudoBuscado.urlImg,
@@ -58,55 +63,52 @@ export default class Manual extends Component{
     }
 
     render(){
+        const { id, titulo, texto, urlImg, nomeContent, texto2 } = this.state;
         return(
             <div className='Manual'>
-                <div>
-                    {this.state.urlImg === '' ? '' : 
-                        <img src={this.state.urlImg} alt=""/>
-                    }
-                    <div>
-                        <p>{this.state.titulo}</p>
-                        <p>{this.state.texto}</p>
-                        <p>{this.state.texto2}</p>
-                        {this.state.nomeContent}
+                <div className='manual-div-content'>
+                    {urlImg === 1 && <img className='manual-img' src={imagem1} alt=""/>}
+                    {urlImg === 2 && <img className='manual-img' src={imagem2} alt=""/>}
+                    {urlImg === 3 && <img className='manual-img' src={imagem3} alt=""/>}
+                    {urlImg === 4 && <img className='manual-img' src={imagem4} alt=""/>}
+                    {urlImg === 5 && <img className='manual-img' src={imagem5} alt=""/>}
+
+                    <div className='manual-textos'>
+                        <h3 className='manual-h3'>{titulo}</h3>
+                        <br/>
+                        <p className='home-p manual-p'>{texto}</p>
+                        <br/>
+                        <p className='home-p manual-p'>{texto2}</p>
                     </div>
                 </div>
-
-
                 <div className='manual-div-numeros'>
-                    <div>
+                    <div className='div-numero-nome'>
                         <p id='1'  onClick={(e) => this.viewDiv(e, 1)} className='numeros-manual numero-apagado' >1</p>
-                        {this.state.id === '1' &&
-                        <p>{this.state.nomeContent}</p>}
+                        {id === 1 &&
+                        <p className='nome-numero'>{nomeContent}</p>}
                     </div>
                     
-
-                    <p id='2' onClick={(e) => this.viewDiv(e, 2)} className='numeros-manual numero-apagado'>2</p>
-                    {this.state.id === '2' &&
-                    <div>
-                        <p>{this.state.nomeContent}</p>
-                    </div>}
-
-                    <p id='3' onClick={(e) => this.viewDiv(e, 3)} className='numeros-manual numero-apagado'>3</p>
-                    {this.state.id === '3' &&
-                    <div>
-                        <p>{this.state.nomeContent}</p>
-                    </div>}
-
-                    <p id='4' onClick={(e) => this.viewDiv(e, 4)} className='numeros-manual numero-apagado'>4</p>
-                    {this.state.id === '4' &&
-                    <div>
-                        <p>{this.state.nomeContent}</p>
-                    </div>}
-
-                    <p id='5' onClick={(e) => this.viewDiv(e, 5)} className='numeros-manual numero-apagado'>5</p>
-                    {this.state.id === '5' &&
-                    <div>
-                        <p>{this.state.nomeContent}</p>
-                    </div>}
+                    <div className='div-numero-nome'>
+                        <p id='2' onClick={(e) => this.viewDiv(e, 2)} className='numeros-manual numero-apagado'>2</p>
+                        {id === 2 &&
+                        <p className='nome-numero'>{nomeContent}</p>}
+                    </div>
+                    <div className='div-numero-nome'>
+                        <p id='3' onClick={(e) => this.viewDiv(e, 3)} className='numeros-manual numero-apagado'>3</p>
+                        {id === 3 &&
+                        <p className='nome-numero'>{nomeContent}</p>}
+                    </div>
+                    <div className='div-numero-nome'>
+                        <p id='4' onClick={(e) => this.viewDiv(e, 4)} className='numeros-manual numero-apagado'>4</p>
+                        {id === 4 &&
+                        <p className='nome-numero'>{nomeContent}</p>}
+                    </div>
+                    <div className='div-numero-nome'>
+                        <p id='5' onClick={(e) => this.viewDiv(e, 5)} className='numeros-manual numero-apagado'>5</p>
+                        {id === 5 &&
+                        <p className='nome-numero'>{nomeContent}</p>}
+                    </div>
                 </div>
-
-                
             </div>
     )}
 }
--- src/components/Manual/contentManual.js
+++ src/components/Manual/contentManual.js
@@ -4,7 +4,7 @@ exports.contentManual = [
         titulo: 'Se liga nisso!',
         texto: 'O jogo começa com uma gincana na sua escola, para te contextualizar sobre os temas das fases, e para a formação das equipes de competição. Ao decorrer do jogo ocorrerão outras gincanas para conhecer pessoas novas, ou até mesmo as que você só conhecia pelas telas.',
         texto2: '',
-        urlImg: '',
+        urlImg: 1,
         nomeContent: 'Explicação'
     },
     {
@@ -12,7 +12,7 @@ exports.contentManual = [
         titulo: 'Deixe seu perfil a sua cara!',
         texto: 'Agora o jogo é virtual e você deve criar seu perfil e colaborar para criação do perfil da sua equipe. Os perfis são customizáveis tendo 4 estéticas diferentes: fofo, medieval, futurista e espacial!',
         texto2: '',
-        urlImg: '../../assets/img/imagem-2.png',
+        urlImg: 2,
         nomeContent: 'Perfis'
     },
     {
@@ -20,7 +20,7 @@ exports.contentManual = [
         titulo: 'Mas... O que que tem nesse jogo?',
         texto: 'Assim que logar você tem uma visão geral das informações mais importantes, como a sua posição no ranking, seu kaês, o desafio atual e a contagem regressiva para o próximo participaê.',
         texto2: '',
-        urlImg: '',
+        urlImg: 3,
         nomeContent: 'Elementos'
     },
     {
@@ -28,7 +28,7 @@ exports.contentManual = [
         titulo: 'Caraca, olha esse post!',
         texto: 'No mural você vai encontrar todas as postagens das outras equipes, como: vídeos cumprindo os desafios, projetos realizados e etc. Ele funciona como um feed e olha que fantástico! Nele você pode aplicar filtros para visualizar o que mais te interessa, como as postagens mais criativas, ou mais visualizadas.',
         texto2: ' E quando seu foco for outro é só editá-los ou excluir os filtros que não quer mais, melhorando a sua experiência.',
-        urlImg: '',
+        urlImg: 4,
         nomeContent: 'Mural'
     },
     {
@@ -36,7 +36,7 @@ exports.contentManual = [
         titulo: 'Dá o play aí!',
         texto: 'Agora é só começar a jogar! Cumpra os desafios que estão disponíveis no perfil dos corujas da sua equipe, lembrando que todo desafio cumprido é avaliado de acordo com alguns critérios para pontuação e neles o jogador também pode desbloquear conquistas no jogo e na escola.',
         texto2: '',
-        urlImg: '',
+        urlImg: 5,
         nomeContent: 'Play'
     },
 ]

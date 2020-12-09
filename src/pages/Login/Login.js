007cf429c37eae6b920cf5bc5ed7a722315c19b3
nadfelipe <nadfelipe.s@gmail.com>
2020-12-09 17:20:06 -0300

    update login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,9 +6,31 @@ export default class Login extends Component{
   render(){
     return (
       <div className="Login">
-        <Link to="/">voltar</Link>
-        <p>Login</p>
-        <Link to="/mural">Acessar</Link>
+        <div className='login-content'>
+          <div className='btn-voltar'>
+          <Link to="/">voltar</Link>
+          </div>
+          <div className='content-login'>
+            <h1 className='login-h1'>Login</h1>
+            <div className='login-form-container'>
+              <form id='form-login' className='form-login' action=''>
+                <div className='input-container'>
+                  <input id='number' type='text' pattern='.+' className='input-login' required/>
+                  <label for='number' >Identificação individual</label>
+                </div>
+                <div className='input-container'>
+                  <input id='senha' type='password' pattern='.+' className='input-login' required/>
+                  <label for='senha'>Senha</label>
+                </div>
+              </form>
+              
+              <Link to="/mural">
+                Acessar
+                <button type='submit' form='form-login' />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

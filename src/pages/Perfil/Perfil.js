76a2ab164e19dcd9f32d712e222d096b394f6417
nadfelipe <nadfelipe.s@gmail.com>
2020-12-12 00:57:19 -0300

    update login + update perfil

--- src/pages/Perfil/Perfil.js
+++ src/pages/Perfil/Perfil.js
@@ -9,6 +9,7 @@ export default class Perfil extends Component{
   constructor(props) {
     super(props);
     this.state = {
+        urlImgFotoPerfil: '',
         nome: '',
         user: '',
         numPubs: '',
@@ -28,6 +29,7 @@ export default class Perfil extends Component{
   }
   componentDidMount(){
     this.setState({
+      urlImgFotoPerfil: '',
       nome: 'Daniel Silva',
       user: '@daniel.silva2',
       numPubs: 23,
@@ -35,6 +37,7 @@ export default class Perfil extends Component{
       ano: 7,
       sala: 'B',
       time: 'Pangeia',
+      urlImgTime: '',
       nivel: 2,
       funcao: 'Coruja',
       classe: 'futurista',
@@ -42,59 +45,81 @@ export default class Perfil extends Component{
     this.getYear();
   }
   render(){
+    let imgClasse;
+    if (this.state.funcao === 'Coruja') {
+      switch (this.state.classe) {
+        case 'futurista':
+          imgClasse = <img src={this.state.urlImgTime} alt='Simbolo do coruja futurista'/>
+          break;
+        case 'medieval':
+          imgClasse = <img src={this.state.urlImgTime} alt='Simbolo do coruja medieval'/>
+          break;
+        case 'fofo':
+          imgClasse = <img src={this.state.urlImgTime} alt='Simbolo do coruja fofo'/>
+          break;
+        case 'espacial':
+          imgClasse = <img src={this.state.urlImgTime} alt='Simbolo do coruja espacial'/>
+          break;
+        default:
+          imgClasse = ''
+          break;
+    }}
     return (
       <div className="Perfil">
         <Nav setar='fora-home' perfil={true}></Nav>
         <div className='top-perfil'>
-
           <div className='content-top-perfil'>
-            <div className='personal-infos'>
-              <div className='left-personal-infos'>
-                <div className='nome-user-infos'>
-                  <h2>{this.state.nome}</h2>
-                  <h3>{this.state.user}</h3>
-                </div>
-                <p>Aparece em: {this.state.numPubs} publicações</p>
-              </div>
-              <div className='right-personal-infos'>
-                <p>N°{this.state.numero}, {this.state.ano}°{this.state.sala} {this.state.anoAtual}</p>
-                <div className='btn-editar-perfil'>
-                  <FontAwesomeIcon icon={faUserCog} size='sm' color='#F23545'/>
-                  <p>Editar Perfil</p>
-                </div>
-              </div>
+            <div className='foto-perfil'>
+              <img src={this.state.urlImgFotoPerfil} alt='Foto de perfil'/>
             </div>
-            <div className='fast-infos-perfil'>
-              <div className='time-info'>
-                <p>{this.state.time}</p>
-                <img src={this.state.urlImgTime} alt='Simbolo do time'/>
+            <div className='infos-perfil'>
+              <div className='personal-infos'>
+                <div className='left-personal-infos'>
+                  <div className='nome-user-infos'>
+                    <h2>{this.state.nome}</h2>
+                    <h3>{this.state.user}</h3>
+                  </div>
+                  <p>Aparece em: {this.state.numPubs} publicações</p>
+                </div>
+                <div className='right-personal-infos'>
+                  <p>N°{this.state.numero}, {this.state.ano}°{this.state.sala} {this.state.anoAtual}</p>
+                  <div className='btn-editar-perfil'>
+                    <FontAwesomeIcon icon={faUserCog} size='sm' color='#F23545'/>
+                    <p>Editar Perfil</p>
+                  </div>
+                </div>
               </div>
-              <div className='nivel-info'>
-                <p>Nível {this.state.nivel}</p>
-              </div>
-              <div className='funcao-info'>
-                <p>{this.state.funcao}</p>
-                {this.state.classe === 'medieval' &&
-                  <img src={this.state.urlImgTime} alt='Simbolo do coruja medieval'/>
-                }
-                {this.state.classe === 'futurista' &&
-                  <img src={this.state.urlImgTime} alt='Simbolo do coruja futurista'/>
-                }
-                {this.state.classe === 'fofo' &&
-                  <img src={this.state.urlImgTime} alt='Simbolo do coruja fofo'/>
-                }
-                {this.state.classe === 'espacial' &&
-                  <img src={this.state.urlImgTime} alt='Simbolo do coruja espacial'/>
-                }
+              <div className='fast-infos-perfil'>
+                <div className='time-info'>
+                  <p>{this.state.time}</p>
+                  <img src={this.state.urlImgTime} alt='Simbolo do time'/>
+                </div>
+                <div className='nivel-info'>
+                  <p>Nível {this.state.nivel}</p>
+                </div>
+                <div className='funcao-info'>
+                  <p>{this.state.funcao}</p>
+                  {imgClasse}
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className='select-page-perfil'>
+        <div className='content-perfil'>
+          <div className='select-page-perfil'>
+            <div className='btn-page-ptns'>
+              <p>Pontuações e conquistas</p>
+            </div>
+            <div className='btn-page-ranking'>
+              <p>Ranking</p>
+            </div>
+            <div className='btn-page-pubs'>
+              <p>Publicações</p>
+            </div>
+          </div>
+          <div className='content-selected-perfil'>
 
-        </div>
-        <div className='content-selected-perfil'>
-
+          </div>
         </div>
         <Footer/>
       </div>

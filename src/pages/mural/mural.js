bfd9103726d804132c7f6d3a3520e44d54189876
nadfelipe <nadfelipe.s@gmail.com>
2020-12-14 04:23:54 -0300

    update mural

--- src/pages/mural/mural.js
+++ src/pages/mural/mural.js
@@ -3,7 +3,6 @@ import './Mural.css';
 import Nav from '../../components/Nav/Nav'
 import Footer from '../../components/Footer/Footer.js'
 import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMedal, faBullhorn, faVectorSquare, faCheck, faPlus, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -12,43 +11,82 @@ export default class Mural extends Component{
     return (
       <div className="Mural">
         <Nav setar='fora-home' mural={true}></Nav>
-        <div>
-          <h2>Bate bola, jogo rápido</h2>
-          <h3>Informações rápidas</h3>
-          <div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-            <div>
-
+        <div className='mural-elements'>
+          <div className='infos'>
+            <h2>Bate bola, jogo rápido</h2>
+            <h3>Informações rápidas</h3>
+            <div className='container-infos'>
+              <div className='content-infos'>
+                <h4>Visão Geral</h4>
+                <div className='content-visao-geral'>
+                  <div>
+                    <FontAwesomeIcon icon={faMedal} size='lg' color='#000'/>
+                    <p>26° no ranking do 7°Ano</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faBullhorn} size='lg' color='#000'/>
+                    <p>83 kaês</p>
+                  </div>
+                </div>
+                <p className='link-card'>veja mais</p>
+              </div>
+              <div className='content-infos'>
+                <h4>Desafios Pendentes</h4>
+                <div className='content-desafios'>
+                  <div>
+                    <FontAwesomeIcon icon={faCheck} size='lg' color='#000'/>
+                    <p>Substituir esse texto aqui depois vei</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faVectorSquare} size='lg' color='#000'/>
+                    <p>Substituir esse texto aqui depois vei</p>
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faVectorSquare} size='lg' color='#000'/>
+                    <p>Substituir esse texto aqui depois vei</p>
+                  </div>
+                </div>
+                <p className='link-card'>ver todos</p>
+              </div>
+              <div className='content-infos'>
+                <h4>FALTAM</h4>
+                <div className='content-contagem'>
+                  <div>
+                    <h5>14</h5>
+                    <h6>dias</h6>
+                  </div>
+                  <p>para o Participaê</p>
+                </div>
+                <p className='link-card'>saiba mais</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <h2>Uau, olha que fantástico</h2>
-          <h3>faça postagens e filtros dinâmicos</h3>
-          <div>
-            <div>
-              <div>
-              <FontAwesomeIcon icon={faPlus} size='sm' color='#F23545'/>
-              <p>Nova Postagem</p>
+          <div className='postagens'>
+            <h2>Uau, olha que fantástico</h2>
+            <h3>faça postagens e filtros dinâmicos</h3>
+            <div className='container-postagens'>
+              <div className='actions-postagens'>
+                <div className='action-postagens action-nova-postagem'>
+                  <FontAwesomeIcon icon={faPlus} size='lg' color='#ffffff85'/>
+                  <p>Nova Postagem</p>
+                </div>
+                <div className='actions-right'>
+                  <div className='action-postagens action-filtro'>
+                    <FontAwesomeIcon icon={faPlus} size='lg' color='#ffffff85'/>
+                    <p>Novo Filtro</p>
+                  </div>
+                  <div className='action-postagens action-filtro'>
+                    <FontAwesomeIcon icon={faPen} size='lg' color='#ffffff85'/>
+                    <p>Editar Filtros</p>
+                  </div>
+                  <div className='action-postagens action-filtro'>
+                    <FontAwesomeIcon icon={faTrash} size='lg' color='#ffffff85'/>
+                    <p>Deletar Filtros</p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <div>
-                  <FontAwesomeIcon icon={faPlus} size='sm' color='#F23545'/>
-                  <p>Novo Filtro</p>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faPen} size='sm' color='#F23545'/>
-                  <p>Editar Filtros</p>
-                </div>
-                <div>
-                  <FontAwesomeIcon icon={faTrash} size='sm' color='#F23545'/>
-                  <p>Deletar Filtros</p>
-                </div>
+              <div className='content-postagens'>
+
               </div>
             </div>
           </div>
